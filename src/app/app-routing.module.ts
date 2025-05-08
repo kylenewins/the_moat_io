6e1e96449bf9b9efbf9126ced6b4a1d894a0679a
Kyle Newins <kylenewins@gmail.com>
2025-05-08 12:30:38 -0400

    home and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { 
+    path: 'home', 
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+  },
+]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [
+  HomeComponent
+
+];
