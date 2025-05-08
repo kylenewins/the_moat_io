5bb5468ad88a1f75f4c466c93ae9b3c6b078f190
Kyle Newins <kylenewins@gmail.com>
2025-05-08 12:47:38 -0400

    music component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
+import { MusicComponent } from './views/music/music.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { 
     path: 'home', 
     loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+  },
+  { 
+    path: 'music', 
+    loadChildren: () => import('./views/music/music.module').then(m => m.MusicModule),
   },
 ]
 ;
@@ -18,6 +23,6 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponent = [
-  HomeComponent
-
+  HomeComponent,
+  MusicComponent
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { HomeComponent } from './views/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
