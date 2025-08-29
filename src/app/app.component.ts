3b8c29af78d1505a6ea242f65a8b16ecd2d104aa
Kyle Newins <kylenewins@gmail.com>
2025-08-28 20:40:17 -0400

    landing page restyled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '/animations.css']
+  styleUrls: ['./app.component.css', '/animations.css', '../desktop-styles.css']
 })
 export class AppComponent {
   title = 'the-moat-io';
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css', '../../animations.css']
+  styleUrls: ['./home.component.css', '../../animations.css', '../../../desktop-styles.css']
 })
 export class HomeComponent implements OnInit {
 
